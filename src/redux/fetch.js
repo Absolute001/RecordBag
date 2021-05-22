7c2a20d4d23a9e7296ac7acf152a41f3a0d7e924
Absolute001 <kshallvari@gmail.com>
2021-05-22 11:19:37 +0200

    now localStorage clears at First render

--- src/redux/fetch.js
+++ src/redux/fetch.js
@@ -19,6 +19,7 @@ const discogsSecret = process.env.REACT_APP_DISCOGS_SECRET;
 /* FETCH FROM YOUTUBE THE CHANNELS RETURNING THE CHANNEL INFOS NEEDED TO RENDER THEM */
 export const fetchChannel = () => {
   return async (dispatch) => {
+    localStorage.clear();
     dispatch({ type: "LOADING_HANDLER", payload: true });
     try {
       const respFirestore = await appFirebase
