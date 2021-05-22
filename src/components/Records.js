4f4518c866e07ad76222454ab4984b0304b77093
Absolute001 <kshallvari@gmail.com>
2021-05-22 13:58:37 +0200

    Renamed YoutubePlayer.js

---
+++ src/components/Records.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { playingVideo, fetchDiscogs } from "../redux/fetch";
+
+const Records = (props) => {
+  const dispatch = useDispatch();
+
+  const dispatchChain = () => {
+    dispatch(fetchDiscogs(props.title));
+    dispatch(
+      playingVideo({
+        videoId: props.videoId,
+        title: props.title,
+        description: props.description,
+        thumbnail: props.thumbnail,
+      })
+    );
+  };
+
+  return (
+    <div
+      className="h-full border-2 border-black py-2 text-black lg:hover:bg-gray-600 
+    lg:hover:text-white lg:transform transition-transform duration-500 lg:hover:-translate-y-2"
+    >
+      <div className="max-w-full my-auto flex" onClick={() => dispatchChain()}>
+        <img
+          src={props.thumbnail}
+          alt="video thumbnail"
+          className="clip-path"
+        />
+        <span
+          className=" max-h-full w-3/6 my-auto text-lg lg:text-2xl"
+          dangerouslySetInnerHTML={{
+            __html:
+              props.title.length > 30
+                ? `${props.title.substring(0, 30)}...`
+                : props.title,
+          }}
+        ></span>
+      </div>
+    </div>
+  );
+};
+
+export default Records;
