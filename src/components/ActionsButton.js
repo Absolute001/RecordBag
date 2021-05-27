519628e74ec6a836f785a084b1f32b97cf330200
Absolute001 <kshallvari@gmail.com>
2021-05-27 23:42:30 +0200

    refactored in more common button for reusability

---
+++ src/components/ActionsButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ActionsButton = (props) => {
+  return (
+    <button
+      onClick={props.onClick}
+      className={`mr-4 outline-none
+       lg:transform transition-transform 
+      duration-500 focus:outline-none lg:hover:-translate-y-2
+        ${props.clicked && "animate animate-ping"}`}
+    >
+      <img src={props.icon} alt={props.alt} className="h-12 w-12 mx-auto" />
+      <h1 className="text-sm p-2">{props.role}</h1>
+    </button>
+  );
+};
+
+export default ActionsButton;
