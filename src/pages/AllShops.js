3e053f9a2157d87faa71ea1054738bca878bead1
Absolute001 <kshallvari@gmail.com>
2021-05-29 18:24:53 +0200

    removed carousel and logic

--- src/pages/AllShops.js
+++ src/pages/AllShops.js
@@ -1,53 +1,42 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from "react";
+import React from "react";
 import ShopThumbnail from "../components/ShopThumbnail";
-import Slider from "react-slick";
 import Hero from "../components/Hero";
-import { fetchChannel } from "../redux/fetch";
-import { useSelector, useDispatch } from "react-redux";
+import HotRecordsContainer from "../container/HotRecordsContainer";
 
-const AllShops = () => {
-  const carouselSettings = {
-    dots: true,
-    fade: true,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    lazyLoad: true,
-  };
-  const dispatch = useDispatch();
-  const channels = useSelector((state) => state.globalState.channel);
-  useEffect(() => {
-    if (channels.length === 0) {
-      dispatch(fetchChannel());
-    }
-  }, []);
+const AllShops = (props) => {
 
   return (
     <>
       <Hero />
-      <section className="lg:mt-16 p-4 max-w-screen-xl mx-auto">
-        <div className="bg-black">
+      <section className="lg:mt-16 max-w-screen-xl mx-auto">
+        <div className="bg-black m-4">
           <h1 className="text-center text-white lg:text-6xl text-4xl pt-4 font-bold">
             SHOPS
           </h1>
           {/* SHOP CAROUSEL */}
-          <div className="lg:mb-16 p-8 xl:px-48 pb-16">
-            <Slider className="md:w-3/6 mx-auto" {...carouselSettings}>
-              {channels.map((channel, index) => (
+          <div
+            className="lg:mb-16 p-8 pb-16 grid grid-cols-2
+           sm:grid-cols-4 gap-2
+           "
+          >
+            {props.channels.map((channel, index) => (
+              <div
+                key={index}
+                className="lg:transform transition-transform
+               duration-500 lg:hover:-translate-y-2"
+              >
                 <ShopThumbnail
-                  key={index}
                   channelId={channel.id}
                   name={channel.title}
                   thumbnail={channel.thumbnail}
                 />
-              ))}
-            </Slider>
+              </div>
+            ))}
           </div>
         </div>
       </section>
+      <HotRecordsContainer />
     </>
   );
 };
