d73edb7f4e7bbe914469601ef022b4d0c99b8272
Absolute001 <kshallvari@gmail.com>
2021-05-29 18:27:13 +0200

    removed useless click handelr for hot record

--- src/redux/utils.js
+++ src/redux/utils.js
@@ -7,25 +7,21 @@ export const clickClipboardHandler = (bool) => {
   return { type: "CLICK_CLIPBOARD_HANDLER", payload: bool };
 };
 
-export const clickAddHandler = (bool) => {
-  return { type: "CLICK_ADD_HANDLER", payload: bool };
-};
-
-export const clickHotRecordHandler = (bool) => {
-  return { type: "CLICK_HOTRECORD_HANDLER", payload: bool };
+export const clickHandler = (bool) => {
+  return { type: "CLICK_HANDLER", payload: bool };
 };
 
 export const pageFlagHandler = (num = 0) => {
   return { type: "PAGE_FLAG_HANDLER", payload: num };
 };
 
-export const pageFlagReset =()=>{
+export const pageFlagReset = () => {
   return { type: "PAGE_FLAG_RESET" };
-}
+};
 
 const utilReducer = (state = utilState, action) => {
   switch (action.type) {
-    case "CLICK_ADD_HANDLER":
+    case "CLICK_HANDLER":
       return {
         ...state,
         clicked: { ...state.clicked, add: action.payload },
@@ -35,11 +31,7 @@ const utilReducer = (state = utilState, action) => {
         ...state,
         clicked: { ...state.clicked, clipboard: action.payload },
       };
-    case "CLICK_HOTRECORD_HANDLER":
-      return {
-        ...state,
-        clicked: { ...state.clicked, hotRecord: action.payload },
-      };
+
     case "PAGE_FLAG_HANDLER":
       return {
         ...state,
