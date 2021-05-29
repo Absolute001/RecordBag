8ac65bfa1719c6c9a6ffcc4034436464085256e9
Absolute001 <kshallvari@gmail.com>
2021-05-29 18:25:37 +0200

    created the Hot Records reducer

---
+++ src/redux/hotRecords.js
@@ -0,0 +1,39 @@
+import appFirebase from "../firebase/firebase";
+
+const hotRecordsState = {
+  hotRecords: [],
+  error: "",
+};
+
+export const fetchHotRecords = () => {
+  return async (dispatch) => {
+    try {
+      const respFirestore = await appFirebase
+        .firestore()
+        .collection("hotRecords")
+        .get();
+      dispatch({
+        type: "FETCH_HOT_RECORDS",
+        payload: respFirestore.docs.map((doc) => doc.data()),
+      });
+    } catch (e) {
+      dispatch({
+        type: "FETCH_HOT_RECORDS_ERROR",
+        payload: e.message,
+      });
+    }
+  };
+};
+
+const hotRecordsReducer = (state = hotRecordsState, action) => {
+  switch (action.type) {
+    case "FETCH_HOT_RECORDS":
+      return { ...state, hotRecords: action.payload };
+    case "FETCH_HOT_RECORDS_ERROR":
+      return { ...state, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default hotRecordsReducer;
