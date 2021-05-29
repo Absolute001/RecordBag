81a99d068c2161293d24161ba53f7951bf00eb66
Absolute001 <kshallvari@gmail.com>
2021-05-29 18:23:51 +0200

    created the Hot Records section!

---
+++ src/container/HotRecordsContainer.js
@@ -0,0 +1,51 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchHotRecords } from "../redux/hotRecords";
+import { Link } from "react-router-dom";
+import Records from "../components/Records";
+
+const HotRecordsContainer = () => {
+  const hotRecords = useSelector((state) => state.hotRecordsState.hotRecords);
+  const hottestRecords = hotRecords.sort((a, b) => b.likes - a.likes);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (hotRecords.length === 0) {
+      dispatch(fetchHotRecords());
+    }
+  }, []);
+
+  return (
+    <section className="mx-auto max-w-screen-xl m-4 p-4">
+      <h1 className="font-bold text-4xl border-b-2 pb-2 border-black">
+        Hot Records{" "}
+        <span role="img" aria-label="sheep">
+          🔥
+        </span>
+        <span className="text-lg"> - Users' Top 10 </span>
+      </h1>
+      <div className="flex flex-col sm:grid grid-cols-2 mt-2">
+        {hottestRecords.map(
+          (record, index) =>
+            index < 10 && (
+              <Link
+                className="mx-2 my-4"
+                key={record.video}
+                to={`/shop/${record.channel}/player/${record.video}`}
+              >
+                <Records
+                  videoId={record.video}
+                  key={index}
+                  thumbnail={record.thumbnail}
+                  title={record.title}
+                  description={record.description}
+                />
+              </Link>
+            )
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default HotRecordsContainer;
