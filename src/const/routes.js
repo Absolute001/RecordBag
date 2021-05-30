ae3fc32f09554b4f462e6345755670e1c79e4c3a
Absolute001 <kshallvari@gmail.com>
2021-05-30 19:34:55 +0200

    routes

---
+++ src/const/routes.js
@@ -0,0 +1,6 @@
+export const collection = "/collection";
+export const shop = "/shop/:channelId";
+export const selectedRecord = "/shop/:channelId/player/:videoId";
+export const login = "/login";
+export const signup = "/signup";
+export const error = "/error";
